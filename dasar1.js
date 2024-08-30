9efaee127dac092263f85421fee0131f447ed0e5
ZsldiNgoding <jajanhujan64@gmail.com>
2024-08-30 09:20:15 +0700

    Frist Upload Cihuyyy

---
+++ dasar1.js
@@ -0,0 +1,42 @@
+var a = 'a';
+let b = 'b';
+const c = 'c';
+
+let nama = 'zaldi'
+let umur = '18'
+let apakahBenar = true;
+let uang = 5000.5
+
+console.log(nama)
+
+let angka1 = 10;
+let angka2 = 20;
+console.log(angka1 + angka2, 'Pertambahan');
+console.log(angka1 - angka2, 'pengurangan');
+console.log(angka1 / angka2, 'Pembagian');
+console.log(angka1 * angka2, 'Perkalian');
+console.log(angka1 % angka2, 'modulus');
+
+let PenampungHasilOperatorAritmatika = angka1 + angka2;
+console.log(PenampungHasilOperatorAritmatika);
+
+PenampungHasilOperatorAritmatika = angka1 - angka2;
+console.log(PenampungHasilOperatorAritmatika);
+
+PenampungHasilOperatorAritmatika = angka1 / angka2;
+console.log(PenampungHasilOperatorAritmatika);
+
+PenampungHasilOperatorAritmatika = angka1 * angka2;
+console.log(PenampungHasilOperatorAritmatika);
+
+PenampungHasilOperatorAritmatika = angka1 % angka2;
+console.log(PenampungHasilOperatorAritmatika);
+
+let firstname = 'Kaka';
+let secondname = 'Zaldi';
+let thirdname = 'Akmal';
+let lastname = 'Fauzan';
+let fullname = firstname + ' ' + secondname + ' ' + thirdname + ' ' + lastname
+
+console.log(firstname)
+console.log(fullname)
